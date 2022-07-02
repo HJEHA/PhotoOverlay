3217db003e52d39bf5435d951a9ae0e2a60b4470
HJEHA <hyhpwang4@gmail.com>
2022-07-02 18:08:43 +0900

    feat: PhotoManager 권한 확인 메서드 추가 #2

--- PhotoOverlay/Presentation/View/PhotosViewController.swift
+++ PhotoOverlay/Presentation/View/PhotosViewController.swift
@@ -7,23 +7,42 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+import RxSwift
 
+final class PhotosViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    private var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configureView()
+        
+        PhotoManager.shared.checkPhotoLibraryAuthorization()
+            .subscribe(onNext: { status in
+                switch status {
+                case .authorized:
+                    print("권한 허용")
+                case .notDetermined:
+                    print("결정되지 않음")
+                case .restricted:
+                    print("앱이 권한이 없음")
+                case .denied:
+                    print("권한 거부")
+                case .limited:
+                    print("제한된 사진")
+                @unknown default:
+                    print("알수 없음")
+                }
+            })
+            .disposed(by: disposeBag)
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension PhotosViewController {
+    private func configureView() {
+        view.backgroundColor = .systemBackground
     }
-    */
-
 }
--- PhotoOverlay/Service/PhotoManager/PhotoManager.swift
+++ PhotoOverlay/Service/PhotoManager/PhotoManager.swift
@@ -6,9 +6,23 @@
 //
 
 import Foundation
+import Photos
+
+import RxSwift
 
 final class PhotoManager {
     static let shared = PhotoManager()
     
     private init() { }
+    
+    /// 앨범 권한 확인 메서드
+    func checkPhotoLibraryAuthorization() -> Observable<PHAuthorizationStatus> {
+        return Observable<PHAuthorizationStatus>.create { emitter in
+            PHPhotoLibrary.requestAuthorization { status in
+                emitter.onNext(status)
+            }
+            
+            return Disposables.create()
+        }
+    }
 }
