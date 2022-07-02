ae524aef463d954f9979165873f8d94a0652cee5
HJEHA <hyhpwang4@gmail.com>
2022-07-02 17:35:20 +0900

    feat: PhotoManager 싱글턴 추가 #2

---
+++ PhotoOverlay/Service/PhotoManager/PhotoManager.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoManager.swift
+//  PhotoOverlay
+//
+//  Created by 황제하 on 2022/07/02.
+//
+
+import Foundation
+
+final class PhotoManager {
+    static let shared = PhotoManager()
+    
+    private init() { }
+}
