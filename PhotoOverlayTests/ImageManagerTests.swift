c461ab5c69f655c523c44576b960ea2d08363f73
HJEHA <hyhpwang4@gmail.com>
2022-07-03 22:34:23 +0900

    test: ImageManagerTests 추가 #4

---
+++ PhotoOverlayTests/ImageManagerTests.swift
@@ -0,0 +1,23 @@
+//
+//  ImageManagerTests.swift
+//  PhotoOverlayTests
+//
+//  Created by 황제하 on 2022/07/03.
+//
+
+import XCTest
+
+final class ImageManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+}
