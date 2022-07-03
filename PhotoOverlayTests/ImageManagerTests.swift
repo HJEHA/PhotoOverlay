73fd821f59a95c3702f1924def9c14c7ecc806c2
HJEHA <hyhpwang4@gmail.com>
2022-07-03 22:46:09 +0900

    test: MockPHImageManager 구현 #4

--- PhotoOverlayTests/ImageManagerTests.swift
+++ PhotoOverlayTests/ImageManagerTests.swift
@@ -6,6 +6,27 @@
 //
 
 import XCTest
+import Photos
+@testable import PhotoOverlay
+
+import RxSwift
+
+final class MockPHImageManager: PHImageManagerable {
+    func requestImage(
+        for asset: PHAsset,
+        targetSize: CGSize,
+        contentMode: PHImageContentMode,
+        options: PHImageRequestOptions?,
+        resultHandler: @escaping (UIImage?, [AnyHashable: Any]?) -> Void
+    ) -> PHImageRequestID {
+        resultHandler(UIImage(), nil)
+        return 0
+    }
+    
+    func cancelImageRequest(_ requestID: PHImageRequestID) {
+        print("\(requestID) 취소")
+    }
+}
 
 final class ImageManagerTests: XCTestCase {
 
