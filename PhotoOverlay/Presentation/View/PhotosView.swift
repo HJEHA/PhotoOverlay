8e128a0ceac8f8c6d2994fbfdc704c4dff01ffb6
HJEHA <hyhpwang4@gmail.com>
2022-07-04 14:19:20 +0900

    feat: PhotosView 구현 #6

---
+++ PhotoOverlay/Presentation/View/PhotosView.swift
@@ -0,0 +1,37 @@
+//
+//  PhotosView.swift
+//  PhotoOverlay
+//
+//  Created by 황제하 on 2022/07/04.
+//
+
+import UIKit
+
+import SnapKit
+
+final class PhotosView: UIView {
+    let photoListCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: UICollectionViewLayout()
+        )
+        
+        return collectionView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func configureView() {
+        addSubview(photoListCollectionView)
+        photoListCollectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
