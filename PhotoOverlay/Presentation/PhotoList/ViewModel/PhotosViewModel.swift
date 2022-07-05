389e3dd9a9a22bd97d6242c520193b70743cb33d
HJEHA <hyhpwang4@gmail.com>
2022-07-05 14:03:37 +0900

    feat: PhotosViewModel 초기 구현 #8
    - ViewModel Protocol 구현

---
+++ PhotoOverlay/Presentation/PhotoList/ViewModel/PhotosViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  PhtosViewModel.swift
+//  PhotoOverlay
+//
+//  Created by 황제하 on 2022/07/05.
+//
+
+import Foundation
+import Photos
+
+final class PhotosViewModel: ViewModel {
+    struct Input { }
+    
+    struct Output { }
+    
+    func transform(_ input: Input) -> Output {
+        return Output()
+    }
+}
