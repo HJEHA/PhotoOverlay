b0182c7bf703200c556bce961b646da6f7e291da
HJEHA <hyhpwang4@gmail.com>
2022-07-05 14:51:51 +0900

    feat: Album Domain Entity 구현 #8

---
+++ PhotoOverlay/Domain/Entities/Album.swift
@@ -0,0 +1,14 @@
+//
+//  Album.swift
+//  PhotoOverlay
+//
+//  Created by 황제하 on 2022/07/05.
+//
+
+import Foundation
+import Photos
+
+struct Album {
+    let title: String
+    var asset: PHAssetCollection?
+}
