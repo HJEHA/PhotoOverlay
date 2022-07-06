9c9f4372dce67683fc421b55d7a686946ccd69b3
HJEHA <hyhpwang4@gmail.com>
2022-07-06 12:38:37 +0900

    feat: AlbumListViewController 생성 #11

---
+++ PhotoOverlay/Presentation/AlbumList/View/AlbumListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AlbumListViewController.swift
+//  PhotoOverlay
+//
+//  Created by 황제하 on 2022/07/06.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+import SnapKit
+
+final class AlbumListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
