596da1c4bf8072a1ce2b56e82053f439ace408c7
HJEHA <hyhpwang4@gmail.com>
2022-07-06 15:41:46 +0900

    feat: DefaultAlbumRepository 구현 #13

---
+++ PhotoOverlay/Data/Repositories/DefaultAlbumRepositoryt.swift
@@ -0,0 +1,25 @@
+//
+//  DefaultAlbumRepositort.swift
+//  PhotoOverlay
+//
+//  Created by 황제하 on 2022/07/06.
+//
+
+import Foundation
+import Photos
+
+import RxSwift
+
+final class DefaultAlbumRepository {
+    let photoManager: PhotoFetchable
+    
+    init(photoManager: PhotoFetchable = PhotoManager.shared) {
+        self.photoManager = photoManager
+    }
+}
+
+extension DefaultAlbumRepository {
+    func fetch() -> Observable<[PHAssetCollection]> {
+        return photoManager.fetchCollections()
+    }
+}
