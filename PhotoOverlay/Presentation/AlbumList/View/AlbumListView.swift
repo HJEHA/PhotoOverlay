42425c1c2e41d2cde19c9d47ee08f902310bab31
HJEHA <hyhpwang4@gmail.com>
2022-07-06 12:45:16 +0900

    feat: AlbumListView 구현 #11

---
+++ PhotoOverlay/Presentation/AlbumList/View/AlbumListView.swift
@@ -0,0 +1,45 @@
+//
+//  AlbumListView.swift
+//  PhotoOverlay
+//
+//  Created by 황제하 on 2022/07/06.
+//
+
+import UIKit
+
+final class AlbumListView: UIView {
+    // MARK: - View Properties
+    
+    let albumListTableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = .gray
+        
+        return tableView
+    }()
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureConstraintsSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+// MARK: - Configure View
+
+extension AlbumListView {
+    private func configureConstraintsSubviews() {
+        
+        // MARK: - Constraints PhotoListCollectionView
+        
+        addSubview(albumListTableView)
+        albumListTableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
--- PhotoOverlay/Presentation/AlbumList/View/AlbumListViewController.swift
+++ PhotoOverlay/Presentation/AlbumList/View/AlbumListViewController.swift
@@ -13,7 +13,31 @@ import SnapKit
 
 final class AlbumListViewController: UIViewController {
 
+    // MARK: - Views
+    
+    private let albumListView = AlbumListView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureView()
+        configureSubViews()
+    }
+}
+
+// MARK: - Configure View
+
+extension AlbumListViewController {
+    private func configureView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func configureSubViews() {
+        view.addSubview(albumListView)
+        
+        albumListView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
     }
 }
