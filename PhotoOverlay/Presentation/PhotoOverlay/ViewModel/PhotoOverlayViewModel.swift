9f22c61815520b28fe55c5dfeb114258f4052ed9
HJEHA <hyhpwang4@gmail.com>
2022-07-07 16:37:20 +0900

    feat: PhotoOverlayViewModel 초기 구현 #19

---
+++ PhotoOverlay/Presentation/PhotoOverlay/ViewModel/PhotoOverlayViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  PhotoOverlayViewModel.swift
+//  PhotoOverlay
+//
+//  Created by 황제하 on 2022/07/07.
+//
+
+import Foundation
+
+final class PhotoOverlayViewModel: ViewModel {
+    
+    // MARK: - Input
+    
+    struct Input {
+        
+    }
+    
+    // MARK: - Output
+    
+    struct Output {
+        
+    }
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Initializer
+    
+    init() {
+        
+    }
+    
+    func transform(_ input: Input) -> Output {
+        
+        return Output()
+    }
+}
