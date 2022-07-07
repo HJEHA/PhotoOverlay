c1b6234825fa4eff066e9d38f3bbcecf6fa3bf7c
HJEHA <hyhpwang4@gmail.com>
2022-07-07 14:41:56 +0900

    feat: PhotoOverlayViewController, PhotoOverlayView 생성 #17

---
+++ PhotoOverlay/Presentation/PhotoOverlay/View/PhotoOverlayView.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoOverlayView.swift
+//  PhotoOverlay
+//
+//  Created by 황제하 on 2022/07/07.
+//
+
+import UIKit
+
+final class PhotoOverlayView: UIView {
+
+}
---
+++ PhotoOverlay/Presentation/PhotoOverlay/View/PhotoOverlayViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoOverlayViewController.swift
+//  PhotoOverlay
+//
+//  Created by 황제하 on 2022/07/07.
+//
+
+import UIKit
+
+final class PhotoOverlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
